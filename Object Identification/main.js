af2e389a18901e4f4a20b2b81b5b9d54408c1631
turanycx <turanycc@gmail.com>
2024-09-10 16:33:28 +0300

    Add files via upload

---
+++ Object Identification/main.js
@@ -0,0 +1,12 @@
+let turan={
+    ad:'Turan',
+    soyad:'Yilancioglu',
+    yas: 12,
+    sinif:12,
+}
+console.log(turan);
+console.log(turan.ad);
+
+console.log(turan.yas);
+turan.yas= 20;
+console.log(turan.yas);
